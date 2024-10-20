e283bc37f6c0e73bfb651a9585171f6037d1ddec
keddo <adu.kedir.abdurahman@gmail.com>
2024-10-20 20:52:27 +0300

    Add analytics coupon payment routs

--- backend/routes/coupon.routes.js
+++ backend/routes/coupon.routes.js
@@ -1,8 +1,10 @@
-import express from 'express';
-import { protectRoute } from '../middleware/auth.middleware';
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getCoupon, validateCoupon } from "../controllers/Coupon.controller.js";
 
 const router = express.Router();
 
-router.get('/', protectRoute)
+router.get("/", protectRoute, getCoupon);
+router.post("/validate", protectRoute, validateCoupon);
 
 export default router;
